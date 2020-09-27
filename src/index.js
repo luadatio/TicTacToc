fc270709d5129b4aa0fc882e16faf6f8db0854ea
luadatio <luadatio@gmail.com>
2020-09-27 14:13:30 +0900

    modified index.js

--- src/index.js
+++ src/index.js
@@ -161,7 +161,7 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i <= lines.length; i++) {
     const [a,b,c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === quares[c]) {
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
